35b92c47f3b8c22f4b89ab6f7441f9fc422a736a
robv <thisisrobv@gmail.com>
2009-08-21 15:31:33 -0500

    Initial commit

---
+++ jquery.input_hint.js
@@ -0,0 +1,32 @@
+(function($) {
+
+	$.fn.input_hint = function(params) {
+		
+		// merge default and user parameters
+		params = $.extend( {attribute: 'title'}, params);
+		
+		// traverse all nodes
+	    return this.each(function() {
+
+	        var $this = $(this);
+
+	        $this.attr('value', $this.attr(params.attribute));
+
+	        $this.focus(function() {
+	            if ($this.attr('value') == $this.attr(params.attribute))
+	                $this.attr('value', '');
+	        }).blur(function() {
+	            if ($this.attr('value') == '')
+	                $this.attr('value', $this.attr(params.attribute));
+	        }).parents('form').submit(function() {
+	            if ($this.attr('value') == $this.attr(params.attribute))
+	                $this.attr('value', '');
+	        });
+
+	    });
+
+		// allow jQuery chaining
+		return this;
+	};
+
+})(jQuery);
