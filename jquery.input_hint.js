3548cde7c90923bd233879f32cf9373fc60f52b6
robv <thisisrobv@gmail.com>
2009-09-18 15:41:41 -0500

    Added "color" parameter, defaults to #999 set to blank for no color change. The color is what the input hint text color will be.

--- jquery.input_hint.js
+++ jquery.input_hint.js
@@ -3,25 +3,25 @@
 	$.fn.input_hint = function(params) {
 
 		// merge default and user parameters
-		params = $.extend({attribute: 'title'}, params);
+		params = $.extend({attribute: 'title', color: '#999'}, params);
 
 		// traverse all nodes
 	    return this.each(function() {
 
 	        var $this = $(this);
 			
-	        if ($this.attr('value') == '')
-	        	$this.attr('value', $this.attr(params.attribute));
+            if ($this.attr('value') == '')
+                $this.attr('value', $this.attr(params.attribute)).css('color', params.color);
 
 	        $this.focus(function() {
 	            if ($this.attr('value') == $this.attr(params.attribute))
-	                $this.attr('value', '');
+	                $this.attr('value', '').css('color', '');
 	        }).blur(function() {
 	            if ($this.attr('value') == '')
-	                $this.attr('value', $this.attr(params.attribute));
+	                $this.attr('value', $this.attr(params.attribute)).css('color', params.color);
 	        }).parents('form').submit(function() {
 	            if ($this.attr('value') == $this.attr(params.attribute))
-	                $this.attr('value', '');
+	                $this.attr('value', '').css('color', '');
 	        });
 
 	    });
