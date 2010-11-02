80d256e6d850939414b75bc1cbc4933073c949f8
robv <thisisrobv@gmail.com>
2010-11-01 23:27:56 -0500

    Input hint plugin is now more of a "placeholder" attribute degradation helper.

--- jquery.input_hint.js
+++ jquery.input_hint.js
@@ -3,28 +3,34 @@
 	$.fn.input_hint = function(params) {
 
 		// merge default and user parameters
-		params = $.extend({attribute: 'title', color: '#999'}, params);
+		params = $.extend({attribute: 'placeholder', color: '#999'}, params);
+		
+		var placeholderSupport = 'placeholder' in document.createElement('input');
+		
+		// Only continue if the browser does not support placeholder
+		if (!placeholderSupport)
+		{
+			// traverse all nodes
+		    return this.each(function() {
 
-		// traverse all nodes
-	    return this.each(function() {
-
-	        var $this = $(this);
+		        var $this = $(this);
 			
-            if ($this.attr('value') == '')
-                $this.attr('value', $this.attr(params.attribute)).css('color', params.color);
-
-	        $this.focus(function() {
-	            if ($this.attr('value') == $this.attr(params.attribute))
-	                $this.attr('value', '').css('color', '');
-	        }).blur(function() {
 	            if ($this.attr('value') == '')
 	                $this.attr('value', $this.attr(params.attribute)).css('color', params.color);
-	        }).parents('form').submit(function() {
-	            if ($this.attr('value') == $this.attr(params.attribute))
-	                $this.attr('value', '').css('color', '');
-	        });
 
-	    });
+		        $this.focus(function() {
+		            if ($this.attr('value') == $this.attr(params.attribute))
+		                $this.attr('value', '').css('color', '');
+		        }).blur(function() {
+		            if ($this.attr('value') == '')
+		                $this.attr('value', $this.attr(params.attribute)).css('color', params.color);
+		        }).parents('form').submit(function() {
+		            if ($this.attr('value') == $this.attr(params.attribute))
+		                $this.attr('value', '').css('color', '');
+		        });
+
+		    });
+		}
 
 		// allow jQuery chaining
 		return this;
